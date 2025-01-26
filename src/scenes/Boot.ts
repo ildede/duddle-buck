1fc4839ef2b8ad1f3aecf985bb2dcea801f2d66b
Yann Le Doare <yann@linuxconsole.org>
2025-01-26 15:14:33 +0100

    New sprites

--- src/scenes/Boot.ts
+++ src/scenes/Boot.ts
@@ -8,12 +8,21 @@ export class Boot extends Scene {
 
   preload() {
     this.load.image('background', 'assets/splash-screen.jpg');
-    this.load.image('gamestart', 'assets/start.png');
-    this.load.image('credits', 'assets/credits.png');
-    this.load.image('realduck', 'assets/real-duck.png');
-    this.load.image('plasticduck', 'assets/plastic-duck.png');
-    this.load.image('sdb', 'assets/sdb.png');
-    this.load.image('cabane', 'assets/cabane-menu.png');
+    this.load.image('gamestart', 'assets/start-credits.png');
+    this.load.image('realduck', 'assets/canards_vrai.png');
+    this.load.image('plasticduck', 'assets/canards_plastique.png');
+    this.load.image('sdb', 'assets/baignoires.png');
+    this.load.spritesheet(
+      'credits',
+      'assets/start-credits.png',
+      {frameWidth: 150, frameHeight: 92}
+    );
+    this.load.spritesheet(
+      'cabane',
+      'assets/baignoires.png',
+      {frameWidth: 300, frameHeight: 205}
+    );
+
   }
 
   create() {
--- src/scenes/MainMenu.ts
+++ src/scenes/MainMenu.ts
@@ -22,44 +22,61 @@ export class MainMenu extends Scene {
       Number(this.game.config.height) / 2,
       'background'
     );
-
+/*
 	  this.gamestart = this.add.image(
-      Number(this.game.config.width) / 8 , 
-      Number(this.game.config.height) -100 , 
+      Number(this.game.config.width) / 8 + 600  , 
+      Number(this.game.config.height) -350 , 
       'gamestart'
     ).on('pointerdown', () => this.scene.start('Game'));
     this.gamestart.setInteractive();
-
+ */
+/*
 	  this.credits = this.add.image(
-      Number(this.game.config.width) / 8 + 300, 
-      Number(this.game.config.height) -100 , 
+      Number(this.game.config.width) / 8 + 900, 
+      Number(this.game.config.height) -350 , 
       'credits'
     ).on('pointerdown', () => this.scene.start('Credits'));
     this.credits.setInteractive();
+*/
+      this.gamestart = this.add.sprite(
+      Number(this.game.config.width) /2 - 124,
+      Number(this.game.config.height) - 270,
+      'credits', 0
+    ).on('pointerdown', () => this.scene.start('Game'));
+    this.gamestart.setInteractive();
+
+      this.credits = this.add.sprite(
+      Number(this.game.config.width)/2 + 124,
+      Number(this.game.config.height) - 270,
+      'credits', 2
+    ).on('pointerdown', () => this.scene.start('Credits'));
+    this.credits.setInteractive();;
+    /*
 
 	  this.sdb = this.add.image(
       Number(this.game.config.width) / 8 + 600, 
-      Number(this.game.config.height) -100 , 
+      Number(this.game.config.height) -550 , 
       'sdb'
     );
 
 	  this.cabane = this.add.image(
       Number(this.game.config.width) / 8 + 900, 
-      Number(this.game.config.height) -100 , 
+      Number(this.game.config.height) -550 , 
       'cabane'
     );
 
 	  this.realduck = this.add.image(
-      Number(this.game.config.width) / 8 + 1200, 
-      Number(this.game.config.height) -100 , 
+      Number(this.game.config.width) / 8 + 700, 
+      Number(this.game.config.height) -750  , 
       'realduck'
     );
 
 	  this.plasticduck= this.add.image(
-      Number(this.game.config.width) / 8 + 1500, 
-      Number(this.game.config.height) -100 , 
+      Number(this.game.config.width) / 8 + 1100, 
+      Number(this.game.config.height) -750 , 
       'plasticduck'
     );
+   */
 
 /*
     this.input.once('pointerdown', () => {
