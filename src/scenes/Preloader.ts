6a2d3d91d8585270ccc3499c89a3f4b8a47ac542
Davide Pedron <dpedron@pubstack.io>
2025-01-26 11:59:58 +0100

    fix boot positioning

--- src/scenes/Preloader.ts
+++ src/scenes/Preloader.ts
@@ -7,13 +7,17 @@ export class Preloader extends Scene {
 
   init() {
     //  We loaded this image in our Boot Scene, so we can display it here
-    this.add.image(512, 384, 'background');
+    this.add.image(
+      Number(this.game.config.width) / 2,
+      Number(this.game.config.height) / 2,
+      'background'
+    );
 
     //  A simple progress bar. This is the outline of the bar.
-    this.add.rectangle(512, 384, 468, 32).setStrokeStyle(1, 0xffffff);
+    this.add.rectangle(Number(this.game.config.width) / 2, Number(this.game.config.height) / 2, 468, 32).setStrokeStyle(1, 0xffffff);
 
     //  This is the progress bar itself. It will increase in size from the left based on the % of progress.
-    const bar = this.add.rectangle(512 - 230, 384, 4, 28, 0xffffff);
+    const bar = this.add.rectangle(Number(this.game.config.width) / 2 - 230, Number(this.game.config.height) / 2, 4, 28, 0xffffff);
 
     //  Use the 'progress' event emitted by the LoaderPlugin to update the loading bar
     this.load.on('progress', (progress: number) => {
@@ -32,22 +36,22 @@ export class Preloader extends Scene {
     this.load.image('dart1', 'dart1.png');
     this.load.image('dart2', 'dart2.png');
     this.load.image('invisible', 'invisible.png');
-    [0,1,2,3,4,5,6,7,8,9,10].forEach((n) => this.load.image(`bolla${n}`, `bolla${n}.png`))
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].forEach((n) => this.load.image(`bolla${n}`, `bolla${n}.png`))
 
     this.load.spritesheet(
       'canard1',
       'canard1.png',
-      { frameWidth: 300, frameHeight: 240 }
+      {frameWidth: 300, frameHeight: 240}
     );
     this.load.spritesheet(
       'canard2',
       'canard2.png',
-      { frameWidth: 300, frameHeight: 240 }
+      {frameWidth: 300, frameHeight: 240}
     );
     this.load.spritesheet(
       'bubble',
       'bubble_pop.png',
-      { frameWidth: 356, frameHeight: 358 }
+      {frameWidth: 356, frameHeight: 358}
     );
 
     this.load.audio('playing-music', 'game-music.ogg');
